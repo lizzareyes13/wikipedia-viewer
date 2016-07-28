84a12b36374f131fa9f6bb98ad9bbb116358dc67
Lizza Reyes <lizzareyes13@gmail.com>
2016-07-27 22:22:19 -0400

    would like to add links to the search results

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const $ = require('jquery');
 
 $.getJSON("http://en.wikipedia.org/w/api.php?callback=?&action=query&format=json&prop=extracts&generator=search&exlimit=10&exintro=1&gsrsearch=Presidents")
 $(document).on("keypress", function(e){
-  console.log(e.keyCode);
-
   if(e.keyCode == 13){
+    $("#searchResults").html("");
+    $("#searchContainer").css("margin-top", "0");
+
     $.getJSON("http://en.wikipedia.org/w/api.php?callback=?"+
     "&action=query&format=json&prop=extracts&generator=search"+
     "&exlimit=10&exintro=1&gsrsearch="+$("#searchBox").val())
@@ -13,8 +14,17 @@ $(document).on("keypress", function(e){
       //resp.query.pages has the unfo I need
       for (var k in resp.query.pages){
         console.log(resp.query.pages[k].title);
-        $("#searchResults").append("<h1>"+ resp.query.pages[k].title + "</h1>");
-        $("#searchResults").append("<p>"+ resp.query.pages[k].extract + "</p>");
+        //$("#searchResults").append("<h1>"+ resp.query.pages[k].title + "</h1>");
+        //$("#searchResults").append("<p>"+ resp.query.pages[k].extract + "</p>");
+        let markupToAppend =
+        `<div class='wiki-result'>
+          <h1>${resp.query.pages[k].title}</h1>
+          <p>
+            ${resp.query.pages[k].extract}
+          </p>
+        </div>`;
+        $("#searchResults").append(markupToAppend);
+
       }
     })
 
